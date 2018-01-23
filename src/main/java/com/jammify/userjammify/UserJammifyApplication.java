a2abb231e134c15f3249d26d0727f0e320cfdb88
Jose <joseenrique86@gmail.com>
2018-01-23 20:16:11 +0000

    Added initial files from Spring start

---
+++ src/main/java/com/jammify/userjammify/UserJammifyApplication.java
@@ -0,0 +1,12 @@
+package com.jammify.userjammify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserJammifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserJammifyApplication.class, args);
+	}
+}

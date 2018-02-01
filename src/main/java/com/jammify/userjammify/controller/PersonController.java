7d42aec90abaf54d18ddb82346fcf77f87fbbc9f
Jose <joseenrique86@gmail.com>
2018-02-01 19:42:44 +0000

    Refactoring Person Controller

--- src/main/java/com/jammify/userjammify/controller/PersonController.java
+++ src/main/java/com/jammify/userjammify/controller/PersonController.java
@@ -3,23 +3,21 @@ package com.jammify.userjammify.controller;
 import com.jammify.userjammify.model.Person;
 import com.jammify.userjammify.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class PersonController {
 
     @Autowired
     private PersonService personService;
 
     @RequestMapping(value = "/user", method = RequestMethod.POST)
-    public ResponseEntity<Person> createUser(@RequestBody Person person){
+    public Person createUser(@RequestBody Person person){
         Person newPerson = personService.createPerson(person);
-        return new ResponseEntity<Person>(newPerson, HttpStatus.CREATED);
+        return newPerson;
     }
 
 }
--- src/test/java/com/jammify/userjammify/controller/PersonControllerTest.java
+++ src/test/java/com/jammify/userjammify/controller/PersonControllerTest.java
@@ -7,7 +7,8 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.Assert.assertEquals;
@@ -23,16 +24,20 @@ public class PersonControllerTest {
     @Autowired
     PersonController personController;
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
 
     @Test
     public void shouldCreatePerson(){
-        Person expected = new Person.PersonBuilder().setId(1L).setName("Pepe").setEmail("pepe@somehost.com").build();
+        Person expected = new Person.PersonBuilder().setId(1L).setName("Pepe")
+                .setEmail("pepe@somehost.com").build();
+        HttpEntity<Person> request = new HttpEntity<>(expected);
 
         when(personService.createPerson(expected)).thenReturn(expected);
+        Person actual =  this.restTemplate.postForObject("/user", request, Person.class);
 
-        ResponseEntity<Person> actual =  personController.createUser(expected);
-
-        assertEquals(expected, actual.getBody());
+        assertEquals(expected, actual);
     }
 
 }
